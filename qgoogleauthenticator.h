3775ac00090bdc6165a23bd098f17f7465032ab8
iMoritz <blaueschlange2@web.de>
2014-01-11 01:50:07 +0100

    Create qgoogleauthenticator.h
    Initial commit

---
+++ qgoogleauthenticator.h
@@ -0,0 +1,24 @@
+#ifndef QGOOGLEAUTHENTICATOR_H
+#define QGOOGLEAUTHENTICATOR_H
+
+#include "qgoogleauthenticator_global.h"
+
+#include <QDateTime>
+#include <QtEndian>
+#include "base32.h"
+#include "hmac.h"
+
+#define SECRET_LENGTH 16
+#define VERIFICATION_CODE_MODULUS (1000*1000) // Six digits
+
+class QGOOGLEAUTHENTICATORSHARED_EXPORT QGoogleAuthenticator
+{
+public:
+    QGoogleAuthenticator();
+
+    static QString getCode(QByteArray secret, quint64 time = 0);
+    static bool checkCode(QString code, QByteArray secret);
+    static QString generate_secret();
+};
+
+#endif // QGOOGLEAUTHENTICATOR_H
